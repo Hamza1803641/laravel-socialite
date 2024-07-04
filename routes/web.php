9c4d12b1047e7e6e580d06c4eadea94779cf198d
Hamza Mehmood <mehmood.hamza777@gmail.com>
2024-07-05 03:28:41 +0500

    add route to access to social login provider

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Two\GithubProvider;
 
 Route::get('/', function () {
     return view('welcome');
@@ -19,3 +20,17 @@ Route::middleware([
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('login/github', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGithub'])->name("login.Github");
+Route::get('login/github/callback', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGithub']);
+
+
+Route::get('login/google', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle'])->name("login.google");
+Route::get('login/google/callback', [App\Http\Controllers\Auth\LoginController::class, 'redirectToGoogle']);
+
+
+Route::get('login/facebook', [App\Http\Controllers\Auth\LoginController::class, 'redirectToFacebook'])->name("login.facebook");
+Route::get('login/facebook/callback', [App\Http\Controllers\Auth\LoginController::class, 'redirectToFacebook']);
+
+
