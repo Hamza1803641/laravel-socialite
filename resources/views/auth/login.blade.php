3762a15952d4975bfeef949b0e3bb2ceab462dd7
Hamza Mehmood <mehmood.hamza777@gmail.com>
2024-07-05 03:03:41 +0500

    change layout and social login button

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,11 +10,24 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
+                        <p style="text-align: center"></p>
 
-                        <div class="row mb-3">
+                        <div style="text-align: center">
+                            <div class="col-md-6 offset-md-3 form-group row" >
+                                <a href="" class="btn btn-danger btn-block"> Login With google</a>
+                                <a href="" class="btn btn-primary btn-block"> Login With Facebook</a>
+                                <a href="" class="btn btn-dark btn-block"> Login With GitHub</a>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus placeholder="E-Mail Address">
+
+                            </div>
+                        </div>
+                        <p style="text-align: center">OR</p>
+
+                        <div class="form-group row">
+                            <div class="col-md-6 offset-md-3">
+                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror"
+                                    name="email" value="{{ old('email') }}" required autocomplete="email" autofocus
+                                    placeholder="E-Mail Address">
 
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
@@ -26,8 +39,10 @@
 
                         <div class="row mb-3">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password" placeholder="password">
+                            <div class="col-md-6 offset-md-3">
+                                <input id="password" type="password"
+                                    class="form-control @error('password') is-invalid @enderror" name="password"
+                                    required autocomplete="current-password" placeholder="password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -38,7 +53,7 @@
                         </div>
 
                         <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-6 offset-md-3">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -50,7 +65,7 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
+                            <div class="col-md-8 offset-md-3">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
